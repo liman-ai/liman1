fc496144c82e46268f7023fc7397937dce4e128a
andapar <160366488+andapar@users.noreply.github.com>
2024-07-25 22:00:54 +0300

    Update jsonUtils.ts

--- lib/jsonUtils.ts
+++ lib/jsonUtils.ts
@@ -1,15 +1,17 @@
-import fs from 'fs'
-import path from 'path'
+// lib/jsonUtils.ts
 
-const allowedEmailsPath = path.join(process.cwd(), 'data', 'allowedEmails.json')
-const blacklistPath = path.join(process.cwd(), 'data', 'sessionBlacklist.json')
+import fs from 'fs';
+import path from 'path';
+
+const allowedEmailsPath = path.join(process.cwd(), 'data', 'allowedEmails.json');
+const blacklistPath = path.join(process.cwd(), 'data', 'sessionBlacklist.json');
 
 export const readAllowedEmails = () => {
-  const jsonData = fs.readFileSync(allowedEmailsPath, 'utf-8')
-  return JSON.parse(jsonData)
-}
+  const jsonData = fs.readFileSync(allowedEmailsPath, 'utf-8');
+  return JSON.parse(jsonData);
+};
 
 export const readBlacklist = () => {
-  const jsonData = fs.readFileSync(blacklistPath, 'utf-8')
-  return JSON.parse(jsonData)
-}
+  const jsonData = fs.readFileSync(blacklistPath, 'utf-8');
+  return JSON.parse(jsonData);
+};
