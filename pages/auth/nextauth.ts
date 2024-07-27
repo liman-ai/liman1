6645a9521b257f0a500e8ff8ca51fe2bcafdb436
andapar <160366488+andapar@users.noreply.github.com>
2024-07-27 19:42:36 +0300

    Update nextauth.ts

--- pages/auth/nextauth.ts
+++ pages/auth/nextauth.ts
@@ -1,4 +1,4 @@
 import NextAuth from 'next-auth';
-import { authOptions } from '../../../auth.config'; // auth.config.ts dosyasının doğru yolunu belirttiğinizden emin olun
+import { authOptions } from 'nextjs/pages/auth/auth.config'; // auth.config.ts dosyasının doğru yolunu belirttiğinizden emin olun
 
 export default NextAuth(authOptions);
