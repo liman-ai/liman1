73fb94725bff69cbd54bf0b56fd80c34add31880
andapar <160366488+andapar@users.noreply.github.com>
2024-07-27 19:58:33 +0300

    Update reset-password.ts

--- pages/api/auth/reset-password.ts
+++ pages/api/auth/reset-password.ts
@@ -1,6 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import nodemailer from 'nodemailer'
 import { kv } from '@vercel/kv'
-import { sendVerificationRequest } from '@/auth.config'
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+})
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
@@ -16,10 +24,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     await kv.set(`reset-token:${token}`, email, { ex: 3600 }) // 1 saatlik geçerlilik süresi
 
     const url = `${process.env.NEXTAUTH_URL}/auth/new-password?token=${token}`
-    await sendVerificationRequest({ identifier: email, url, provider: { server: '', from: process.env.EMAIL_FROM } })
+    const message = {
+      to: email,
+      from: process.env.EMAIL_FROM,
+      subject: `Parola Sıfırlama`,
+      text: `Parolanızı sıfırlamak için şu bağlantıyı kullanın: ${url}`,
+      html: `<p>Parolanızı sıfırlamak için şu bağlantıyı kullanın: <a href="${url}">Parola Sıfırla</a></p>`,
+    }
 
-    return res.status(200).json({ message: 'Password reset email sent' })
+    transporter.sendMail(message, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error)
+        return res.status(500).json({ error: 'Error sending email' })
+      } else {
+        console.log('Email sent:', info.response)
+        return res.status(200).json({ message: 'Password reset email sent' })
+      }
+    })
+  } else {
+    res.status(405).json({ error: 'Method not allowed' })
   }
-
-  res.status(405).json({ error: 'Method not allowed' })
 }
