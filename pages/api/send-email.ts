a78916101c70e9b9d40ac7b132512482883c1b03
andapar <160366488+andapar@users.noreply.github.com>
2024-07-28 00:48:07 +0300

    Update send-email.ts

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -1,35 +1,29 @@
-// pages/api/send-email.ts
-import { NextApiRequest, NextApiResponse } from 'next';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const { email, url, from } = req.body;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const { email, url, from } = req.body;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-    const message = {
-      to: email,
-      from,
-      subject: `Sign in to ${new URL(url).host}`,
-      text: `Sign in to ${new URL(url).host}\n\n${url}\n\n`,
-      html: `<p>Sign in to <strong>${new URL(url).host}</strong></p><p><a href="${url}">Sign in</a></p>`,
-    };
+  const message = {
+    to: email,
+    from,
+    subject: `Sign in to ${new URL(url).host}`,
+    text: `Sign in to ${new URL(url).host}\n\n${url}\n\n`,
+    html: `<p>Sign in to <strong>${new URL(url).host}</strong></p><p><a href="${url}">Sign in</a></p>`,
+  };
 
-    try {
-      await transporter.sendMail(message);
-      res.status(200).json({ message: 'Email sent' });
-    } catch (error) {
-      console.error('Error sending email:', error);
-      res.status(500).json({ error: 'Error sending email' });
-    }
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
+  try {
+    await transporter.sendMail(message);
+    res.status(200).json({ message: 'Email sent' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error sending email' });
   }
 }
