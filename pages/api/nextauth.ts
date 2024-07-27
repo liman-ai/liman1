efca033334437f18650810c9cfea000796fcb4a7
andapar <160366488+andapar@users.noreply.github.com>
2024-07-27 19:39:39 +0300

    Delete pages/api/nextauth.ts

--- pages/api/nextauth.ts
+++
@@ -1,4 +0,0 @@
-import NextAuth from 'next-auth';
-import { authOptions } from '../../../auth.config'; // auth.config.ts dosyasının doğru yolunu belirttiğinizden emin olun
-
-export default NextAuth(authOptions);
