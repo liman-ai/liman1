7d94451c0dd265a8f5524825608a5fe19e5abdf1
liman-ai <1993limanai@gmail.com>
2024-07-29 13:32:26 +0300

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,60 +1,42 @@
-import type { NextAuthOptions } from 'next-auth';
-import EmailProvider from 'next-auth/providers/email';
+import type { NextAuthConfig } from 'next-auth'
 
-export const sendVerificationRequest = async ({
-  identifier: email,
-  url,
-  provider: { server, from },
-}: {
-  identifier: string;
-  url: string;
-  provider: { server: string; from: string };
-}) => {
-  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/send-email`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email, url, from }),
-  });
-
-  if (!response.ok) {
-    console.error('Error sending email:', await response.text());
-  }
-};
-
-export const authOptions: NextAuthOptions = {
+export const authConfig = {
   secret: process.env.AUTH_SECRET,
   pages: {
     signIn: '/login',
-    newUser: '/signup',
+    newUser: '/signup'
   },
-  providers: [
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: parseInt(process.env.EMAIL_SERVER_PORT || '587'),
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-      sendVerificationRequest,
-    }),
-  ],
   callbacks: {
+    async authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user
+      const isOnLoginPage = nextUrl.pathname.startsWith('/login')
+      const isOnSignupPage = nextUrl.pathname.startsWith('/signup')
+
+      if (isLoggedIn) {
+        if (isOnLoginPage || isOnSignupPage) {
+          return Response.redirect(new URL('/', nextUrl))
+        }
+      }
+
+      return true
+    },
     async jwt({ token, user }) {
       if (user) {
-        token.id = user.id;
+        token = { ...token, id: user.id }
       }
-      return token;
+
+      return token
     },
     async session({ session, token }) {
       if (token) {
-        session.user.id = token.id;
+        const { id } = token as { id: string }
+        const { user } = session
+
+        session = { ...session, user: { ...user, id } }
       }
-      return session;
-    },
+
+      return session
+    }
   },
-};
+  providers: []
+} satisfies NextAuthConfig
